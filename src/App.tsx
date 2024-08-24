c3c9e8c7a12e4f761f39348703a9ac20114055c9
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-08-24 23:25:47 +0100

    add css styling to app

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,24 @@ import Section from './components/Section';
 import SubSection from './components/SubSection';
 import MathExample from './components/MathExample';
 import NavBar from './components/NavBar';
+import './styling/App.css'; // Make sure to import your CSS file
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <NavBar />
-      <Section title="Introduction">
-        <p>This is a sample React app that mimics the appearance of a LaTeX document.</p>
-      </Section>
+      <div className="content-container">
+        <Section title="Introduction">
+          <p>This is a sample React app that mimics the appearance of a LaTeX document.</p>
+        </Section>
 
-      <Section title="Mathematics">
-        <SubSection title="Integrals">
-          <p>Here is an example of an integral:</p>
-          <MathExample />
-        </SubSection>
-      </Section>
+        <Section title="Mathematics">
+          <SubSection title="Integrals">
+            <p>Here is an example of an integral:</p>
+            <MathExample />
+          </SubSection>
+        </Section>
+      </div>
     </div>
   );
 };
