f4457217e080619e202aa7cc8c7b8fc1a68c97db
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-08-24 23:06:28 +0100

    small navbar fixes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,42 +2,25 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Typography } from '@mui/material';
 import '../styling/NavBar.css';
-
-// Create a custom theme to override default Material UI styles
-const theme = createTheme({
-  typography: {
-    fontFamily: 'cmu_serifroman, serif',
-    button: {
-      textTransform: 'none', // Remove the default uppercase transformation
-      fontWeight: 'bold',
-    },
-  },
-  palette: {
-    primary: {
-      main: '#f5f5f5', // Background color similar to LaTeX documents
-    },
-    text: {
-      primary: '#333', // Darker text color for readability
-    },
-  },
-});
 
 const NavBar: React.FC = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <AppBar position="static" color="primary" className="navbar">
-        <Toolbar>
+    <AppBar position="sticky" className="navbar">
+      <Toolbar className="toolbar">
+        <Typography variant='h4' className="navbar-brand">
+          Majd Yousof
+        </Typography>
+        <div className="navbar-buttons">
           <Button color="inherit" href="#home">Home</Button>
           <Button color="inherit" href="#about">About</Button>
           <Button color="inherit" href="#services">Services</Button>
           <Button color="inherit" href="#contact">Contact</Button>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
+        </div>
+      </Toolbar>
+    </AppBar>
   );
 };
 
 export default NavBar;
-
