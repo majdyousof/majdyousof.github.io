9867e7a2f513232326f72b21bdee4d0c4e3bb653
majdyousof <117210724+majdyousof@users.noreply.github.com>
2024-08-26 22:42:19 +0100

    amend tags

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -13,5 +13,12 @@ export const projects = [
     technologies: ['React', 'Typescript', 'CSS', 'Latex'],
     link: 'https://github.com/majdyousof/majdyousof.github.io',
   },
+  {
+    title: 'Heathrow Surface Access Dashboard',
+    description:
+      'A dashboard built using Python, Pandas, Plotly and Streamlit to visualize the surface access to Heathrow Airport. This was done to accompany a group report that scored 83.77%.',
+    technologies: ['Python', 'Pandas', 'Plotly', 'Streamlit'],
+    link: 'https://heathrow-surface-access-assessment.streamlit.app/',
+  },
   // Add more projects here
 ];
